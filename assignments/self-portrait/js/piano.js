3f5e107945e89f73bd67a10cedb775d9d6b906b5
gildnej <gildnej@wwu.edu>
2019-10-07 10:43:14 -0700

    finished product with working piano and nice looking room

--- assignments/self-portrait/js/piano.js
+++ assignments/self-portrait/js/piano.js
@@ -1,4 +1,4 @@
-var whitekeys = ["C3","D3","E3","F3","G3","A4","B4","C4","D4","E4","F4","G4","A5","B5","C5","D5","E5","F5","G5","A6","B6"];
+var whitekeys = ["C3","D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4","C5","D5","E5","F5","G5","A5","B5"];
 var blackkeys = ["C-3","D-3","F-3","G-3","A-3","C-4","D-4","F-4","G-4","A-4","C-5","D-5","F-5","G-5","A-5"];
 
 var playnote = function(id){
@@ -52,3 +52,20 @@ $(function() {
     var width = div.width();
     div.css('height', width*.5);
 });
+
+//Randomize Colors
+$(function(){
+    var floor = $("#floor");
+    var wall1 = $("#wall");
+    var wall2 = $("#wall2");
+
+    var redVal = Math.floor(Math.random() * 256);
+
+    var color1 = "rgb("+redVal+",0,0)"
+    var color2 = "rgb(0,"+((redVal+191)%255)+","+((redVal+64)%255)+")"
+    var color3 = "rgb(0,"+((redVal+64)%255)+","+((redVal+191)%255)+")"
+
+    floor.css("background-color",color1);
+    wall1.css("background-color",color2);
+    wall2.css("background-color",color3);
+})
