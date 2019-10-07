77b9f0c2d3c8eb6c5ddfd464557756ebea7f23aa
gildnej <gildnej@wwu.edu>
2019-10-06 20:35:52 -0700

    modified piano and added some objects to the room

--- assignments/self-portrait/js/piano.js
+++ assignments/self-portrait/js/piano.js
@@ -1,14 +1,22 @@
-var whitekeys = ["c3","d3","e3","f3","g3","a4","b4","c4","d4","e4","f4","g4","a5","b5","c5","d5","e5","f5","g5","a6","b6"];
-var blackkeys = ["c-3","d-3","f-3","g-3","a-3","c-4","d-4","f-4","g-4","a-4","c-5","d-5","f-5","g-5","a-5"];
+var whitekeys = ["C3","D3","E3","F3","G3","A4","B4","C4","D4","E4","F4","G4","A5","B5","C5","D5","E5","F5","G5","A6","B6"];
+var blackkeys = ["C-3","D-3","F-3","G-3","A-3","C-4","D-4","F-4","G-4","A-4","C-5","D-5","F-5","G-5","A-5"];
 
 var playnote = function(id){
     notes[id].play();
+    setTimeout(function(){
+        notes[id].pause();
+        notes[id].currentTime = 0;
+    }, 500);
+    
 }
 
 var notes = new Object();
 
 $(function(){
     whitekeys.forEach((id) =>{
+        notes[id] = new Audio("keys/"+id+".mp3");
+    })
+    blackkeys.forEach((id) =>{
         notes[id] = new Audio("keys/"+id+".mp3");
     })
 });
@@ -38,3 +46,9 @@ $(function(){
     })
 })
 
+
+$(function() {
+    var div = $('.roomgrid');
+    var width = div.width();
+    div.css('height', width*.5);
+});
