49651e0b44972e3dcbff5d4a5fe7e076cf37d3a1
joegildner <gildnej@wwu.edu>
2019-10-23 10:48:38 -0700

    change change change

---
+++ js/vis_sleep.js
@@ -0,0 +1,258 @@
+var saveColors = [];
+
+var margin = {top: 20, right: 30, bottom: 35, left: 50};
+
+var width = 960 - margin.left - margin.right,
+height = 800 - margin.top - margin.bottom;
+
+var buildChart = function(){
+    
+    var filterData = function(jsonfile){
+        return jsonfile.map(function(part){
+            part.sleep = part.sleep.filter(function(value){
+                return value.infoCode==0;
+            })
+            return part;
+        })
+    }
+    
+    var stackData = function(jsonData){
+        var all_participant_nights = [];
+        var x = 0;
+        var parts = jsonData.map(function(person){
+            var all_nights = person.sleep.map(function(night, i){
+                var prev = 0;
+                var single_night = night.levels.data.map(function(phase, j, dataArr){
+                    var obj = {x:x, level:phase.level, y:phase.seconds, y0: prev}
+                    prev += phase.seconds;
+                    return obj;
+                });
+                x++;
+                return single_night;
+            });
+            all_participant_nights = all_participant_nights.concat(all_nights);
+        })
+        return all_participant_nights;
+    }
+    
+    var getYScale = function(data1, data2){
+        
+        var yMax1 = d3.max(data1, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  });
+        var yMax2 = d3.max(data2, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  });
+        
+        var totalMax;
+        
+        yMax1 > yMax2 ? totalMax = yMax1 : totalMax = yMax2;
+        
+        var y = d3.scaleLinear()
+        .domain([0, totalMax])
+        .range([height, 0]);
+        
+        return y;
+    }
+
+    function sortData(jsonData){
+        jsonData.sort(function(a,b){
+            return (a[a.length-1].y0+a[a.length-1].y)-(b[b.length-1].y0+b[b.length-1].y)
+        });
+        console.log(jsonData);
+    }
+    
+    var graphData = function(dataset, yScale, divId, title){
+        d3.select(divId).append("h1").text(title);
+        var svg = d3.select(divId)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
+        var x = d3.scaleLinear()
+        .domain([0,dataset.length])
+        .range([0,width])
+        
+        var y = yScale;
+        
+        var nights = svg.selectAll("g")
+        .data(dataset)
+        .enter().append("g")
+        .attr("class",function(d,i){
+            return "night"+i;
+        });
+        
+        var rect = nights.selectAll("rect")
+        .data(function(d){return d})
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { 
+            return x(d.x)
+        })
+        .attr("y", function(d) { return y(d.y0 + d.y); })
+        .attr("width", width/dataset.length)
+        .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+        .attr("id", function(d){return d.level});
+        
+        // Add scales to axis
+        var x_axis = d3.axisBottom()
+        .scale(x);
+        
+        var y_axis = d3.axisLeft(y)
+        .tickFormat(function(d){
+            return d3.format("d")(d/3600);
+        })
+        
+        //Append group and insert axis
+        svg.append("g")
+        .attr("transform", "translate(0, " + height  +")")
+        .call(x_axis);
+        
+        svg.append("g")
+        .attr("transform", "translate(0, 0)")
+        .call(y_axis);
+        
+        svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Time Asleep (hours)");  
+        
+        svg.append("text")             
+        .attr("transform",
+        "translate(" + (width/2) + " ," + 
+        (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("");
+        
+    }
+    
+    var drawLegend = function(){
+        svg = d3.select("svg")
+        var phases = ["wake", "deep", "light", "rem"];
+        var colors = phases.map((c) => {
+            return {phase:c, color:getComputedStyle(document.getElementById(c)).getPropertyValue("fill")}
+        })
+        
+        console.log(colors);
+        
+        // Draw legend
+        var legend = svg.selectAll(".legend")
+        .data(phases)
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 19 + ")"; });
+        
+        legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .attr("id", function(d) {return d})
+        .on("mouseover", function(d){
+            handleOver(d,phases);
+        })
+        .on("mouseout", function(d){
+            handleOff(colors);
+        })
+        
+        legend.append("text")
+        .attr("x", width + 5)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function(d){ return d});
+    }
+    
+    var handleOver = function(d, phases){
+        phases.forEach((change) => {
+            if(change!=d){
+                document.querySelectorAll("#"+change).forEach(function(d){
+                    d.style.fill = "gray";
+                })
+            }
+            if(d!='light'){
+                document.querySelectorAll("#unknown").forEach(function(d){
+                    d.style.fill = "gray";
+                })
+            }
+        })
+    }
+    
+    var handleOff = function(colors){
+        colors.forEach((segment) => {
+            document.querySelectorAll("#"+segment.phase).forEach(function(d){
+                d.style.fill = segment.color;
+            })
+        })
+        document.querySelectorAll("#unknown").forEach(function(d){
+            d.style.fill = "cyan";
+        })
+    }
+    
+    
+    var getColor = function(d){
+        switch(d.level) {
+            case "wake":
+            return "#FF0000"
+            break;
+            case "light":
+            return "#00FF00"
+            break;
+            case "deep":
+            return "#0000FF"
+            break;
+            case "rem":
+            return "#00FFFF"
+            break;
+            default:
+            return "#FFFFFF"
+        } 
+    }
+    
+    //Procedures
+
+    function createViz(){
+        var ntpFiltered = filterData(ntpjson);
+    var ntpNights = stackData(ntpFiltered);
+    ntpNights.sort(compare);
+    ntpNights.forEach((a,i)=>{
+        a.forEach((b)=>{
+            b.x=i;
+        })
+    })
+    
+    var ndvFiltered = filterData(ndvjson);
+    var ndvNights = stackData(ndvFiltered);
+
+    ndvNights.sort(compare);
+    ndvNights.forEach((a,i)=>{
+        a.forEach((b)=>{
+            b.x=i;
+        })
+    })
+    
+    var yScale = getYScale(ndvNights, ntpNights);
+    
+    
+    graphData(ntpNights, yScale, "#col1", "Neurotypical Sleep Habits");
+    graphData(ndvNights, yScale, "#col2", "Neurodiverse Sleep Habits");
+
+    
+    drawLegend();
+}
+    
+    
+
+    function compare(a,b){
+        var aH = (a[a.length-1]['y0']+a[a.length-1]['y']);
+        var bH = (b[b.length-1]['y0']+b[b.length-1]['y']);
+        return aH-bH;
+    };
+    
+    createViz();
+}
+
+loadAllData(buildChart);
+
+
+
---
+++ temp/competencies/dynamic-webpages/assignments/self-portrait/js/piano.js
@@ -0,0 +1,72 @@
+var whitekeys = ["C3","D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4","C5","D5","E5","F5","G5","A5","B5"];
+var blackkeys = ["C-3","D-3","F-3","G-3","A-3","C-4","D-4","F-4","G-4","A-4","C-5","D-5","F-5","G-5","A-5"];
+
+var playnote = function(id){
+    notes[id].play();
+    setTimeout(function(){
+        notes[id].pause();
+        notes[id].currentTime = 0;
+    }, 500);
+    
+}
+
+var notes = new Object();
+
+$(function(){
+    whitekeys.forEach((id) =>{
+        notes[id] = new Audio("keys/"+id+".mp3");
+    })
+    blackkeys.forEach((id) =>{
+        notes[id] = new Audio("keys/"+id+".mp3");
+    })
+});
+
+$(function() {
+    var div = $('.keygrid');
+    var width = div.width();
+    div.css('height', width*.3);
+});
+
+//Build white keys
+$(function(){
+    whitekeys.forEach((id, i) => {
+      $("#"+id).css({"grid-column":i+1,"grid-row":1}).click(function(){playnote(id)});
+    })
+})
+
+//Build black keys
+$(function(){
+    var place = 0;
+    var i=0;
+    blackkeys.forEach((id) => {
+      $("#"+id).css({"grid-column":i+1,"grid-row":1}).click(function(){playnote(id)});
+      i++;
+      if((i+5)%7 == 0 || (i+1)%7 == 0) i++;
+    })
+})
+
+
+$(function() {
+    var div = $('.roomgrid');
+    var width = div.width();
+    div.css('height', width*.5);
+});
+
+//Randomize Colors
+$(function(){
+    var floor = $("#floor");
+    var wall1 = $("#wall");
+    var wall2 = $("#wall2");
+
+    var redVal = Math.floor(Math.random() * 256);
+    var blueVal = Math.floor(Math.random() * (256-redVal));
+    var greenVal = Math.floor(Math.random() * (256-blueVal));
+
+    var color1 = "rgb("+redVal+","+greenVal+","+blueVal+")"
+    var color2 = "rgb("+redVal+","+((greenVal+191)%255)+","+((blueVal+64)%255)+")"
+    var color3 = "rgb("+redVal+","+((greenVal+64)%255)+","+((blueVal+191)%255)+")"
+
+    floor.css("background-color",color1);
+    wall1.css("background-color",color2);
+    wall2.css("background-color",color3);
+})
